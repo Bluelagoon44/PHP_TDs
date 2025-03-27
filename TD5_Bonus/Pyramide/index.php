742edd4dae276918ce508d2568dd8bd30e54affc
Brendan <brendan.viale@gmail.com>
2025-03-27 15:35:30 +0100

    TD5 Bonus - Pyramide

---
+++ TD5_Bonus/Pyramide/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <a href="pyramide.php">Pyramide</a>
+        <form action="pyramide.php" method="POST">
+            <input type="number" name="etages" placeholder="Nombre d'étages voulus" />
+            <button>Créer la pyramide</button>
+        </form>
+    </body>
+</html>
